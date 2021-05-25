b3540b17c3f095d44b6b745821e32c76c89d017c
Khoi Phan <khoiphanngoc89@gmail.com>
2021-05-25 21:31:13 +0700

    feat: + update star.component.html + add pipe + models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { FormsModule } from '@angular/forms';
+import { ConvertToSpacePipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   declarations: [
     ProductListComponent,
-    AppComponent
+    AppComponent,
+    ConvertToSpacePipe,
+    StarComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   bootstrap: [AppComponent]
 })
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from "@angular/core";
-import { IProduct } from '../models/product';
+import { IProduct } from './product';
+import { ProductService } from "./product.service";
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
 })
 
 export class ProductListComponent implements OnInit {
@@ -11,31 +12,37 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 100;
     imageMargin: number = 2;
     isShowImage: boolean = false;
-    listFilter: string = 'cart';
-    products: IProduct[] = [
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2021",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "assets/images/leaf_rake.png"
-        },
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2021",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.8,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-        }
-    ]
+
+    constructor(private productService: ProductService) {
+
+    }
+
+    private _listFilter: string = '';
+    get listFilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value: string) {
+        this._listFilter = value;
+        this.filteredProducts = this.performFilter(this.listFilter);
+    }
+
+    performFilter(filteredBy: string) : IProduct[] {
+        if(!filteredBy) return this.products;
+        const filtered = filteredBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+            product.productName.toLocaleLowerCase().includes(filtered));
+    }
+
+    filteredProducts: IProduct[] = [];
+    products: IProduct[] = [];
 
     ngOnInit() {
-        
+        this.products = this.productService.getProducts();
+        this.filteredProducts = this.products.filter(n => n);
+    }
+
+    onRatingClicked(value: string): void {
+        this.pageTitle = 'Product List: ' + value;
+
     }
 }
