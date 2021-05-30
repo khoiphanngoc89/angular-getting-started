69d28ec0495985b403f59de7923527aaf66d4492
Khoi Phan <khoiphanngoc89@gmail.com>
2021-05-30 22:30:12 +0700

    feat: update use httpClient

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,33 +1,28 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
+
 import { IProduct } from './product';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ProductService {
+    private productUrl = 'api/products/products.json'
 
-    getProducts(): IProduct[] {
-        return [
-            {
-                "productId": 1,
-                "productName": "Leaf Rake",
-                "productCode": "GDN-0011",
-                "releaseDate": "March 19, 2021",
-                "description": "Leaf rake with 48-inch wooden handle.",
-                "price": 19.95,
-                "starRating": 3.2,
-                "imageUrl": "assets/images/leaf_rake.png"
-            },
-            {
-                "productId": 2,
-                "productName": "Garden Cart",
-                "productCode": "GDN-0023",
-                "releaseDate": "March 18, 2021",
-                "description": "15 gallon capacity rolling garden cart",
-                "price": 32.8,
-                "starRating": 4.2,
-                "imageUrl": "assets/images/garden_cart.png"
-            }
-        ]
+    /**
+     *
+     */
+    constructor(private http: HttpClient) {
+    }
+    getProducts(): Observable<IProduct[]> {
+        return this.http.get<IProduct[]>(this.productUrl).
+        pipe(tap(data => console.log('All: ', JSON.stringify(data)))).
+        catchError(this.handleError);
+    }
+
+    handleError(err: HttpErrorResponse) {
+        
     }
 }
